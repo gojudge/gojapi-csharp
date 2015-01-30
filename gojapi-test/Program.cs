b391ecd6ee17709f709f157d0b6463274e4a1126
Rex Lee <duguying2008@gmail.com>
2015-01-30 23:47:34 +0800

    initialize
    initial library project, define library classes and testing case

---
+++ gojapi-test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using gojapi;
+
+namespace gojapi_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestJudgerHTTP("oj.duguying.net",1005);
+        }
+
+        static void TestJudgerHTTP(string host, int port) 
+        {
+            JudgerHTTP jdg = new JudgerHTTP(host,port);
+        }
+    }
+}
---
+++ gojapi/JudgerHTTP.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace gojapi
+{
+    public class JudgerHTTP
+    {
+        public JudgerHTTP(string host, int port) 
+        {
+        }
+    }
+}
